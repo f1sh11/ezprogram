454e63ef3ea5a55b0f4ba27f5d5318069ed92883
Abcwhy <1810521486@qq.com>
2025-03-13 13:26:41 +0800

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.log('MongoDB connected');
     // Start the server only when the database connection is successful
     const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
 
   .catch(err => {
