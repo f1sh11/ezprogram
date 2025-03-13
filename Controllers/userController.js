38a764410f0652242de85644f800ad5e1674218c
Abcwhy <1810521486@qq.com>
2025-03-13 13:29:24 +0800

    Update userController.js

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,10 +1,10 @@
-// 获取当前登录用户的信息
+// Get information about the currently logged in user
 exports.getProfile = (req, res) => {
-    // authMiddleware 已验证用户并将用户对象附加在 req.user
+    // authMiddleware has authenticated the user and attached the user object to req.user
     if (!req.user) {
       return res.status(401).json({ message: 'Not authorized' });
     }
-    // 返回用户信息（已经在中间件中过滤掉了密码等敏感信息）
+    // Return user information (sensitive information such as passwords has been filtered out in the middleware)
     res.json(req.user);
   };
   
