9e585524b98e443ac67175ce38db1071cd66dc36
Abcwhy <1810521486@qq.com>
2025-03-13 13:26:28 +0800

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());        // Parsing JSON Request Body
 app.use(cors());                // Allow cross-domain requests
 
-// 连接 MongoDB 数据库
+// Connecting to a MongoDB Database
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB connected');
