9ad112e450013ab9fe19dd8ab904f6512bb54f77
f1sh11 <jijoinyucheng@gmail.com>
2025-03-15 15:50:15 +0800

    修正错误

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  identifier: { type: String, required: true, unique: true }, // This can be a username or email address
-  password:   { type: String, required: true },
-  role:       { type: String, enum: ['user', 'admin'], default: 'user', required: true }
-});
-
-module.exports = mongoose.model('User', userSchema);
